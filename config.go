84577e73ec7696ddb9ab2f05bf3b848fe71459a3
Jesse Alford <jalford@pivotal.io>
2018-04-24 14:36:54 -0600

    Delete obsolete/unused method and tests [Incidental to #157058526]
    Signed-off-by: Ben Fuller <bfuller@pivotal.io>

--- config.go
+++ config.go
@@ -1,38 +1,12 @@
 package blackbox
 
 import (
-	"errors"
 	"io/ioutil"
 	"os"
-	"time"
 
 	"github.com/cloudfoundry/blackbox/syslog"
 	"gopkg.in/yaml.v2"
 )
-
-type Duration time.Duration
-
-func (d *Duration) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	var num int64
-	if err := unmarshal(&num); err == nil {
-		*d = Duration(num)
-		return nil
-	}
-
-	var str string
-	if err := unmarshal(&str); err != nil {
-		return errors.New("invalid duration; must be string or number")
-	}
-
-	duration, err := time.ParseDuration(str)
-	if err != nil {
-		return err
-	}
-
-	*d = Duration(duration)
-
-	return nil
-}
 
 type SyslogConfig struct {
 	Destination syslog.Drain `yaml:"destination"`
