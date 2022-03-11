a4000690728cfc23e0c715d6a5a139af170502a0
Ben Fuller <benjaminf@vmware.com>
2022-03-11 19:48:16 +0000

    wip replace syslog
    Signed-off-by: Rebecca Roberts <robertsre@vmware.com>
    Co-authored-by: Rebecca Roberts <robertsre@vmware.com>

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module code.cloudfoundry.org/blackbox
 go 1.16
 
 require (
+	code.cloudfoundry.org/go-loggregator/v8 v8.0.5
+	code.cloudfoundry.org/tlsconfig v0.0.0-20200131000646-bbe0f8da39b3
 	github.com/nxadm/tail v1.4.8
 	github.com/onsi/ginkgo v1.16.2
 	github.com/onsi/gomega v1.12.0
-	github.com/stretchr/testify v1.7.0
 	github.com/tedsuo/ifrit v0.0.0-20191009134036-9a97d0632f00
 	github.com/ziutek/syslog v0.0.0-20180426113420-8a9fdf1a8529
 	gopkg.in/yaml.v2 v2.4.0
--- syslog/drainer.go
+++ syslog/drainer.go
@@ -1,11 +1,17 @@
 package syslog
 
 import (
+	"crypto/tls"
 	"crypto/x509"
-	"errors"
 	"io/ioutil"
 	"log"
+	"net"
+	"strconv"
 	"time"
+
+	"code.cloudfoundry.org/tlsconfig"
+
+	"code.cloudfoundry.org/go-loggregator/v8/rfc5424"
 )
 
 type Drain struct {
@@ -21,71 +27,93 @@ type Drainer interface {
 const ServerPollingInterval = 5 * time.Second
 
 type drainer struct {
+	conn           net.Conn
+	dialFunction   func() (net.Conn, error)
 	errorLogger    *log.Logger
-	logger         *Logger
 	hostname       string
 	structuredData string
 }
 
 func NewDrainer(errorLogger *log.Logger, drain Drain, hostname, structuredData string) (*drainer, error) {
-	err := errors.New("non-nil")
-	var logger *Logger
-	var certPool *x509.CertPool
+	var dialFunction func() (net.Conn, error)
+	var tlsConf *tls.Config
 	if len(drain.CA) != 0 {
 		ca, err := ioutil.ReadFile(drain.CA)
 		if err != nil {
 			errorLogger.Printf("Error reading ca certificate: %s \n", err.Error())
 			return nil, err
 		}
-		certPool = x509.NewCertPool()
+		certPool := x509.NewCertPool()
 		certPool.AppendCertsFromPEM(ca)
-	}
-
-	for err != nil {
-		logger, err = Dial(
-			hostname,
-			drain.Transport,
-			drain.Address,
-			certPool,
-			30*time.Second,
-			30*time.Second,
-			99990,
+		tlsConf, err = tlsconfig.Build(
+			tlsconfig.WithExternalServiceDefaults(),
+		).Client(
+			tlsconfig.WithAuthority(certPool),
 		)
-
 		if err != nil {
-			errorLogger.Printf("Connection error: %s, Will retry in %d \n", err.Error(), ServerPollingInterval)
-			time.Sleep(ServerPollingInterval)
+			errorLogger.Printf("Error creating tls config: %s \n", err.Error())
+			return nil, err
 		}
 	}
 
-	if err != nil {
-		return nil, err
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: time.Second * 60 * 3,
+	}
+	switch drain.Transport {
+	case "tls":
+		dialFunction = func() (net.Conn, error) {
+			return tls.DialWithDialer(dialer, "tcp", drain.Address, tlsConf)
+		}
+	case "tcp":
+		dialFunction = func() (net.Conn, error) {
+			return dialer.Dial("tcp", drain.Address)
+		}
+	case "udp":
+		dialFunction = func() (net.Conn, error) {
+			return dialer.Dial("udp", drain.Address)
+		}
 	}
 
 	return &drainer{
-		logger:         logger,
 		hostname:       hostname,
 		structuredData: structuredData,
 		errorLogger:    errorLogger,
+		dialFunction:   dialFunction,
 	}, nil
 }
 
 func (d *drainer) Drain(line string, tag string) error {
-	d.logger.Packets <- Packet{
-		Severity:       SevInfo,
-		Facility:       LogUser,
-		StructuredData: d.structuredData,
-		Hostname:       d.hostname,
-		Tag:            tag,
-		Time:           time.Now(),
-		Message:        line,
+	m := rfc5424.Message{
+		Priority:  rfc5424.Info,
+		Timestamp: time.Now(),
+		Hostname:  d.hostname,
+		AppName:   tag,
+		ProcessID: "rs2",
+		Message:   []byte(line),
+		// StructuredData: d.structuredData,
 	}
 
-	select {
-	case err := <-d.logger.Errors:
-		d.errorLogger.Printf("Error sending syslog packet: %s", err)
+	binary, err := m.MarshalBinary()
+	if err != nil {
+		d.errorLogger.Printf("Error marshalling syslog: %s \n", err.Error())
 		return err
-	default:
-		return nil
 	}
+	for d.conn == nil {
+		conn, err := d.dialFunction()
+		if err != nil {
+			d.errorLogger.Printf("Error connecting: %s \n", err.Error())
+			time.Sleep(time.Second)
+		}
+		if conn != nil {
+			d.conn = conn
+		}
+	}
+	d.conn.SetWriteDeadline(time.Now().Add(time.Second * 30))
+	_, err = d.conn.Write([]byte(strconv.Itoa(len(binary)) + " " + string(binary)))
+	if err != nil {
+		d.errorLogger.Printf("Error writing: %s \n", err.Error())
+		d.conn = nil
+	}
+	return nil
 }
