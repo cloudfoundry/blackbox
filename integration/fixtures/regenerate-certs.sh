78e8ea885822e7647eb734374f6d0005b4b432b1
Carson Long <12767276+ctlong@users.noreply.github.com>
2024-01-18 14:00:58 -0800

    fix(integration): Regenerate fixtures
    Regenerates the certs in fixtures, which had all expired.
    
    Updates regenerate-certs.sh to generate certs that don't expire for 10
    years.

--- integration/fixtures/regenerate-certs.sh
+++ integration/fixtures/regenerate-certs.sh
@@ -7,11 +7,11 @@ this_dir="$(cd $(dirname $0) && pwd)"
 pushd "$this_dir"
 
 rm -rf out
-certstrap init --common-name "ca" --passphrase ""
+certstrap init --common-name "ca" --passphrase "" --expires "10 years"
 certstrap request-cert --common-name "server" --domain "localhost" --ip "127.0.0.1" --passphrase ""
-certstrap sign server --CA "ca"
+certstrap sign server --CA "ca" --expires "10 years"
 certstrap request-cert --common-name "server-bad" --passphrase ""
-certstrap sign server-bad --CA "ca"
+certstrap sign server-bad --CA "ca" --expires "10 years"
 
 mv -f out/* ./
 rm -rf out
